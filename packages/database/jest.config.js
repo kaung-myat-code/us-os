33a59c973cd681dc3f845a57e8397cbba56533c3
Kmpg <kmpg.dev@gmail.com>
2026-07-29 00:26:32 +0800

    Add Jest test tooling to packages/database

---
+++ packages/database/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  testRegex: '.*\\.(spec|test)\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+};
